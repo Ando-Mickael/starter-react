10d3607aa6d57789f58021bfdadb21b39c1ed146
Ando Mickael Ramahenina <ando.mickael25@gmail.com>
2023-06-24 16:46:15 +0300

    generalize chart

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import MyTable from '../components/generalize/MyTable';
+import { MyPieChart } from '../components/generalize/MyChart';
 
 export default function IndexPage() {
     const columns = ["nom", "age"];
@@ -17,10 +18,34 @@ export default function IndexPage() {
             age: 24
         },
     ];
-    
+
+    const labels = ['bebe', 'enfant', 'ado', 'adulte', 'vieux'];
+    const obj = {
+        bebe: 12,
+        enfant: 19,
+        ado: 3,
+        adulte: 5,
+        vieux: 2,
+    };
+
     return (
-        <div>
-            <MyTable columns={columns} data={data} />
-        </div>
+        <main>
+            <div className="container">
+                <div className="row mb-3">
+                    <div className="card">
+                        <div className="card-body">
+                            <MyTable columns={columns} data={data} />
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="card">
+                        <div className="card-body">
+                            <MyPieChart labels={labels} obj={obj} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     );
 }
